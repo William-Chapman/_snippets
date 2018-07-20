9a54a76847fc84d97c1ef0be837de8fc7134f8e4
William Chapman <chapman.williamt@gmail.com>
2018-07-20 13:11:05 -0400

    updated continue function

--- ContinueMethod.cs
+++ ContinueMethod.cs
@@ -2,16 +2,26 @@ using System;
 
 class Library
 {
+    /// <summary>
+    /// This file is an example and snippet of my continue function
+    /// it askes the user if they would like to continue using the program 
+    /// based on their input we have three options, continue, stop or ask for valid input
+    /// </summary>
     static void Main()
     {
-        //This File is an example of my Continue Method
-        Continue();
+        do
+        {
+            //code for program goes here
+        }
+        //call continue method
+        while (Continue() == 1);
     }
 
-    static void Continue()
+    static int Continue()
     {
         string response;
-        for (int i = 0; i < 5; i++)
+        int situ = 3;
+        while (situ == 3)
         {
             Console.WriteLine("Continue? (y/n): ");
             response = Console.ReadLine().ToLower();
@@ -19,32 +29,63 @@ class Library
             if (response == "y" || response == "yes")
             {
                 //if yes, restart at main
-                Main();
+                situ = 1;
             }
             else if (response == "n" || response == "no")
             {
                 //if no, exit
-                i = 5;
-                return;
+                situ = 0;
+            }
+            else if (response != "n" || response != "no" || response != "y" || response != "yes")
+            {
+                situ = 3;
             }
 
-            if (i <= 2 && response != "n" && response != "no" && response != "y" && response != "yes")
+            if (situ == 3)
             {
-                //if the user enters neither, inform and repeat
-                Console.WriteLine("Sorry, I could not understand your response.");
-                Console.WriteLine("Please enter a valid response.");
-            }
-            else if (i == 3 && response != "n" && response != "no" && response != "y" && response != "yes")
-            {
-                Console.WriteLine("Sorry, I could not understand your response.");
-                Console.WriteLine("If a vaild response is not entered on this attempt the program will end");
-            }
-            else if (i == 4 && response != "n" && response != "no" && response != "y" && response != "yes")
-            {
-                Console.WriteLine("Sorry, I could not understand your response.");
-                Console.WriteLine("You have run out of continue attempts and the program has terminated.");
+                Console.WriteLine("Please enter valid response.");
             }
         }
-        return;
+        return situ;
     }
+
+    //static void Continue()
+    //{
+    //    string response;
+    //    for (int i = 0; i < 5; i++)
+    //    {
+    //        Console.WriteLine("Continue? (y/n): ");
+    //        response = Console.ReadLine().ToLower();
+
+    //        if (response == "y" || response == "yes")
+    //        {
+    //            //if yes, restart at main
+    //            Main();
+    //        }
+    //        else if (response == "n" || response == "no")
+    //        {
+    //            //if no, exit
+    //            i = 5;
+    //            return;
+    //        }
+
+    //        if (i <= 2 && response != "n" && response != "no" && response != "y" && response != "yes")
+    //        {
+    //            //if the user enters neither, inform and repeat
+    //            Console.WriteLine("Sorry, I could not understand your response.");
+    //            Console.WriteLine("Please enter a valid response.");
+    //        }
+    //        else if (i == 3 && response != "n" && response != "no" && response != "y" && response != "yes")
+    //        {
+    //            Console.WriteLine("Sorry, I could not understand your response.");
+    //            Console.WriteLine("If a vaild response is not entered on this attempt the program will end");
+    //        }
+    //        else if (i == 4 && response != "n" && response != "no" && response != "y" && response != "yes")
+    //        {
+    //            Console.WriteLine("Sorry, I could not understand your response.");
+    //            Console.WriteLine("You have run out of continue attempts and the program has terminated.");
+    //        }
+    //    }
+    //    return;
+    //}
 }
